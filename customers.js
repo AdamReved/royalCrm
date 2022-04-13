80fe29e02c0e78aeff97a559a1f9f75099d1369c
AdamReved <97845450+AdamReved@users.noreply.github.com>
2022-04-13 19:35:06 +0300

    first commit

---
+++ customers.js
@@ -0,0 +1,21 @@
+const customers = [];
+
+function addCustomer() {
+  const args = process.argv;
+  const username = args.slice(2);
+
+  if (!username || username.length === 0) {
+    throw ("Error: userName is empty!");
+  }
+
+  const tempPwd = Math.floor(Math.random() * 100000);
+
+  customers.push({
+    username: username,
+    password: tempPwd,
+  });
+
+  customers.forEach((customer) => {
+    console.log(`Ok - customer ${customer} was added`);
+  });
+}
