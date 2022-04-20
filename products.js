999d9bbbdda2b46f66525c32d46386932be96719
AdamReved <97845450+AdamReved@users.noreply.github.com>
2022-04-20 14:03:54 +0300

    producs.js and orders.js tested and added

---
+++ products.js
@@ -0,0 +1,22 @@
+const products = [];
+
+function addProduct() {
+  const product = process.argv.slice(2);
+
+  if (!product || product.length === 0) {
+    throw "Error: order is empty!";
+  }
+
+  const productId = Math.floor(Math.random() * 100000);
+
+  products.push({
+    product: product,
+    productId: productId,
+  });
+
+  products.forEach((product) => {
+    console.log(`ok. product: ${product.product} with temporary id: ${product.productId}.`);
+  });
+}
+
+addProduct();
